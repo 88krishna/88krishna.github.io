f00588c514db1f272d0e690b8818d7a562f42da5
Krishna S <krishna.s.ece@gmail.com>
2020-03-31 21:18:29 -0700

    Put two images for mom site

--- mom/js/myscripts.js
+++ mom/js/myscripts.js
@@ -1,8 +1,19 @@
-$("#clickaction").click(function (e) {
+$("#clickaction1").click(function (e) {
   e.preventDefault();
   $("#celebration").toggle();
+  $("#pillu-normal").toggle();
+  $("#pillu-clicked").toggle();
+});
+
+$("#clickaction2").click(function (e) {
+  e.preventDefault();
+  $("#celebration").toggle();
+  $("#pillu-normal").toggle();
+  $("#pillu-clicked").toggle();
 });
 
 function hideCelebrationContent() {
   $("#celebration").hide();
+  $("#pillu-normal").toggle();
+  $("#pillu-clicked").toggle();
 }
